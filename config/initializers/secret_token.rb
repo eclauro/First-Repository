16404f5c4fcce2a8737f74862610fc344c9529ec
Lauro Jr <laurojr@lauro-jrs-macbook-pro.local>
2011-02-05 11:23:59 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freemark::Application.config.secret_token = '7acc85fae7511da20f3f42eefc2aa79483ed1d0773d3484e0ebabb7950947441fb98130649b6661870d02941b97d9ba3685942e93dcfef5b0ff0a791845fa1fc'
